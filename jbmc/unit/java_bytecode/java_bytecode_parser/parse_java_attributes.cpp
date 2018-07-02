9df72a8a12a0240f818ce126a0271b151c96a55c
Jeannie Moulton <jeannie.moulton@diffblue.com>
2018-07-02 12:27:38 +0100

    fixup! Updates tests with review comments

--- jbmc/unit/java_bytecode/java_bytecode_parser/parse_java_attributes.cpp
+++ jbmc/unit/java_bytecode/java_bytecode_parser/parse_java_attributes.cpp
@@ -12,71 +12,175 @@
 #include <java_bytecode/java_types.h>
 #include <testing-utils/catch.hpp>
 
-// See
-// https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.7.16.1
 SCENARIO(
   "java_bytecode_parse_attributes",
   "[core][java_bytecode][java_bytecode_parser]")
 {
-  GIVEN("Some class files in the class path")
+  const symbol_tablet &new_symbol_table =
+    load_java_class("InnerClasses", "./java_bytecode/java_bytecode_parser");
+  GIVEN("Some public class files in the class path with inner classes")
   {
     WHEN("Parsing the InnerClasses attribute for a public inner class")
     {
-      const symbol_tablet &new_symbol_table =
-        load_java_class("InnerClasses", "./java_bytecode/java_bytecode_parser");
-
       THEN("The class should be marked as public")
       {
         const symbolt &class_symbol =
-          *new_symbol_table.lookup("java::InnerClasses$PublicInnerClass");
+          new_symbol_table.lookup_ref("java::InnerClasses$PublicInnerClass");
         const java_class_typet java_class =
           to_java_class_type(class_symbol.type);
-        REQUIRE(java_class.get_inner_class() == ID_is_inner_class);
+        REQUIRE(java_class.get_is_inner_class());
         REQUIRE(java_class.get_access() == ID_public);
       }
     }
     WHEN("Parsing the InnerClasses attribute for a package private inner class")
     {
-      const symbol_tablet &new_symbol_table =
-        load_java_class("InnerClasses", "./java_bytecode/java_bytecode_parser");
-
       THEN("The class should be marked as default")
       {
         const symbolt &class_symbol =
-          *new_symbol_table.lookup("java::InnerClasses$DefaultInnerClass");
+          new_symbol_table.lookup_ref("java::InnerClasses$DefaultInnerClass");
         const java_class_typet java_class =
           to_java_class_type(class_symbol.type);
-        REQUIRE(java_class.get_inner_class() == ID_is_inner_class);
+        REQUIRE(java_class.get_is_inner_class());
         REQUIRE(java_class.get_access() == ID_default);
       }
     }
     WHEN("Parsing the InnerClasses attribute for a protected inner class")
     {
-      const symbol_tablet &new_symbol_table =
-        load_java_class("InnerClasses", "./java_bytecode/java_bytecode_parser");
-
       THEN("The class should be marked as protected")
       {
         const symbolt &class_symbol =
-          *new_symbol_table.lookup("java::InnerClasses$ProtectedInnerClass");
+          new_symbol_table.lookup_ref("java::InnerClasses$ProtectedInnerClass");
         const java_class_typet java_class =
           to_java_class_type(class_symbol.type);
-        REQUIRE(java_class.get_inner_class() == ID_is_inner_class);
+        REQUIRE(java_class.get_is_inner_class());
         REQUIRE(java_class.get_access() == ID_protected);
       }
     }
     WHEN("Parsing the InnerClasses attribute for a private inner class")
     {
-      const symbol_tablet &new_symbol_table =
-        load_java_class("InnerClasses", "./java_bytecode/java_bytecode_parser");
-
       THEN("The class should be marked as private")
       {
         const symbolt &class_symbol =
-          *new_symbol_table.lookup("java::InnerClasses$PrivateInnerClass");
+          new_symbol_table.lookup_ref("java::InnerClasses$PrivateInnerClass");
         const java_class_typet java_class =
           to_java_class_type(class_symbol.type);
-        REQUIRE(java_class.get_inner_class() == ID_is_inner_class);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_private);
+      }
+    }
+  }
+  GIVEN("Some package-private class files in the class path with inner classes")
+  {
+    WHEN("Parsing the InnerClasses attribute for a public inner class")
+    {
+      THEN("The class should be marked as public")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDefault$PublicInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_public);
+      }
+    }
+    WHEN("Parsing the InnerClasses attribute for a package private inner class")
+    {
+      THEN("The class should be marked as default")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDefault$DefaultInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_default);
+      }
+    }
+    WHEN("Parsing the InnerClasses attribute for a protected inner class")
+    {
+      THEN("The class should be marked as protected")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDefault$ProtectedInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_protected);
+      }
+    }
+    WHEN("Parsing the InnerClasses attribute for a private inner class")
+    {
+      THEN("The class should be marked as private")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDefault$PrivateInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_private);
+      }
+    }
+  }
+  GIVEN(
+    "Some package-private class files in the class path with deeply nested "
+    "inner classes")
+  {
+    WHEN(
+      "Parsing the InnerClasses attribute for a public doubly-nested inner "
+      "class")
+    {
+      THEN("The class should be marked as public")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDeeplyNested$SinglyNestedClass$"
+          "PublicDoublyInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_public);
+      }
+    }
+    WHEN(
+      "Parsing the InnerClasses attribute for a package private doubly-nested "
+      "inner class")
+    {
+      THEN("The class should be marked as default")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDeeplyNested$SinglyNestedClass$"
+          "DefaultDoublyInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_default);
+      }
+    }
+    WHEN(
+      "Parsing the InnerClasses attribute for a protected doubly-nested inner "
+      "class")
+    {
+      THEN("The class should be marked as protected")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDeeplyNested$SinglyNestedClass$"
+          "ProtectedDoublyInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
+        REQUIRE(java_class.get_access() == ID_protected);
+      }
+    }
+    WHEN(
+      "Parsing the InnerClasses attribute for a private doubly-nested inner "
+      "class")
+    {
+      THEN("The class should be marked as private")
+      {
+        const symbolt &class_symbol = new_symbol_table.lookup_ref(
+          "java::InnerClassesDeeplyNested$SinglyNestedClass$"
+          "PrivateDoublyInnerClass");
+        const java_class_typet java_class =
+          to_java_class_type(class_symbol.type);
+        REQUIRE(java_class.get_is_inner_class());
         REQUIRE(java_class.get_access() == ID_private);
       }
     }
