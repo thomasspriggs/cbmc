19fa996a0908bbde789c884b26f5164dda65eda4
Thomas Spriggs <thomas.spriggs@diffblue.com>
2019-11-03 17:01:23 +0000

    Add array unit test

--- jbmc/unit/java_bytecode/signatures/parser.cpp
+++ jbmc/unit/java_bytecode/signatures/parser.cpp
@@ -21,8 +21,19 @@ TEST_CASE("Parse type variable java signature",
       ->identifier == "fish");
 }
 
+TEST_CASE("Parse array of type variable java signature",
+  "[core][java_bytecode][java_signature_parser]")
+{
+  REQUIRE(
+    std::dynamic_pointer_cast<java_signature_type_variablet>(
+      std::dynamic_pointer_cast<java_signature_array_typet>(
+        java_signature_parse("[Tfish;"))
+        ->element_type)
+      ->identifier == "fish");
+}
+
 TEMPLATE_TEST_CASE(
-  "Parse primitive java signatures",
+  "Parse primitive array java signatures",
   "[core][java_bytecode][java_signature_parser]",
   (java_signature_primitivet<'B'>),
   (java_signature_primitivet<'C'>),
@@ -33,5 +44,7 @@ TEMPLATE_TEST_CASE(
   (java_signature_primitivet<'S'>))
 {
   REQUIRE(std::dynamic_pointer_cast<test_typet>(
-    java_signature_parse({test_typet::signature()})));
+    std::dynamic_pointer_cast<java_signature_array_typet>(
+      java_signature_parse(std::string{"["} + test_typet::signature()))
+      ->element_type));
 }
